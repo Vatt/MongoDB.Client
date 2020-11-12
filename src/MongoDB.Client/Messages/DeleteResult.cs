3b07b8d781b65b9b62c6022bf8e23e0c051fe71c
evgeny.p <anarh2404@outlook.com>
2020-11-12 22:21:18 +0300

    model

--- src/MongoDB.Client/Messages/DeleteResult.cs
+++ src/MongoDB.Client/Messages/DeleteResult.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
-using MongoDB.Client.Bson.Document;
 using MongoDB.Client.Bson.Serialization.Attributes;
 
 namespace MongoDB.Client.Messages
 {
+    [BsonSerializable]
     public class DeleteResult : IParserResult
     {
-        public BsonDocument Document { get; set; }
+        [BsonElementField(ElementName = "n")]
+        public int N { get; set; }
+        
+        [BsonElementField(ElementName = "ok")]
+        public double Ok { get; set; }
     }
 }
