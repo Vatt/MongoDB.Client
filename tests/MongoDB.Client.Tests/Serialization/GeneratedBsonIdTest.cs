06de979769df6ddb22d45f13c2f4afdeb163718b
Gamover <gamover@outlook.com>
2020-11-20 23:26:48 +0500

    BsonId test

---
+++ tests/MongoDB.Client.Tests/Serialization/GeneratedBsonIdTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Client.Bson.Serialization;
+using MongoDB.Client.Tests.Serialization.TestModels;
+using Xunit;
+
+namespace MongoDB.Client.Tests.Serialization
+{
+    public class GeneratedBsonIdTest : BaseSerialization
+    {
+        [Fact]
+        public async Task BsonIdTest()
+        {
+            var model = new BsonIdModel(Guid.NewGuid(), Int32.MaxValue);
+            SerializersMap.TryGetSerializer<BsonIdModel>(out var serializer);
+            var result = await RoundTripAsync(model, serializer);
+
+            Assert.Equal(model, result);
+        }
+    }
+}
