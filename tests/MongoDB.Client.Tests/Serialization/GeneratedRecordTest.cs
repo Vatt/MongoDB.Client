a6c75328a2b8b6143a2be40a90ba51d0313cce99
Gamover <gamover@outlook.com>
2020-11-24 00:21:41 +0500

    records test

---
+++ tests/MongoDB.Client.Tests/Serialization/GeneratedRecordTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Client.Bson.Document;
+using MongoDB.Client.Bson.Serialization;
+using MongoDB.Client.Tests.Serialization.TestModels;
+using Xunit;
+
+namespace MongoDB.Client.Tests.Serialization
+{
+    public class GeneratedRecordTest : BaseSerialization
+    {
+        [Fact]
+        public async Task RecordTest()
+        {
+            var model = new RecordModel(42, 42, 42, "42", Guid.NewGuid());
+            model.Document = new BsonDocument("42", "42");
+            SerializersMap.TryGetSerializer<RecordModel>(out var serializer);
+            var result = await RoundTripAsync(model, serializer);
+
+            Assert.Equal(result, model);
+        }
+        [Fact]
+        public async Task RecordWithConstructorTest()
+        {
+            var model = new RecordWithConstructor(42, 42, 42, "42", Guid.NewGuid());
+            model.Document = new BsonDocument("42", "42");
+            SerializersMap.TryGetSerializer<RecordWithConstructor>(out var serializer);
+            var result = await RoundTripAsync(model, serializer);
+
+            Assert.Equal(result, model);
+        }
+    }
+}
--- tests/MongoDB.Client.Tests/Serialization/TestModels/RecordModel.cs
+++ tests/MongoDB.Client.Tests/Serialization/TestModels/RecordModel.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using MongoDB.Client.Bson.Document;
 using MongoDB.Client.Bson.Serialization.Attributes;
 
 namespace MongoDB.Client.Tests.Serialization.TestModels
 {
     [BsonSerializable]
-    public sealed record RecordModel(int A, long B, double C, string D, Guid E);
+    public sealed record RecordModel(int A, long B, double C, string D, Guid E)
+    {
+        public BsonDocument Document { get; set; }
+    }
+    [BsonSerializable]
+    public record RecordWithConstructor
+    {
+        public int A;
+        public long B;
+        public double C;
+        public string D;
+        public Guid E;
+        public BsonDocument Document;
+        [BsonConstructor]
+        public RecordWithConstructor(int A, long B, double C, string D, Guid E)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.D = D;
+            this.E = E;
+        }
+    }
 }
