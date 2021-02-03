99d60f58497c49f6b3a152f57ce46494f3ea59a4
anarh2404 <anarh2404@outlook.com>
2021-02-03 21:12:43 +0300

    add script

---
+++ tests/MongoDb.Client.WebApi/script.js
@@ -0,0 +1,9 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function () {
+  var url = 'http://localhost:5000/api/test';
+
+  let res = http.get(url);
+  check(res, { 'status was 200': (r) => r.status == 200 });
+}
