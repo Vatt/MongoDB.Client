41ddd347c9dcb086cd9fd781d12f71dcfd42cc6c
anarh2404 <anarh2404@outlook.com>
2021-02-04 20:26:47 +0300

    wip

--- tests/MongoDb.Client.WebApi/Mongo/New/NewGeoipRepository.cs
+++ tests/MongoDb.Client.WebApi/Mongo/New/NewGeoipRepository.cs
@@ -1,37 +1,9 @@
-﻿using MongoDB.Client;
-using MongoDB.Client.Bson.Document;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace MongoDb.Client.WebApi.Mongo
+﻿namespace MongoDb.Client.WebApi.Mongo
 {
-    public class NewGeoipRepository : IMongoRepository<GeoIp>
+    public class NewGeoipRepository : BaseNewRepository<GeoIp>
     {
-        private readonly MongoCollection<GeoIp> _collection;
-
-        public NewGeoipRepository(INewMongo mongo)
+        public NewGeoipRepository(INewMongo mongo) : base(mongo, "GeoipCollection")
         {
-            _collection = mongo.GetCollection<GeoIp>("GeoipCollection");
-        }
-
-        public IAsyncEnumerable<GeoIp> GetAllAsync()
-        {
-            return _collection.Find(BsonDocument.Empty);
-        }
-
-        public ValueTask<GeoIp> GetAsync(string id)
-        {
-            return _collection.Find(new BsonDocument("_id", new BsonObjectId(id))).FirstOrDefaultAsync();
-        }
-
-        public ValueTask InsertAsync(GeoIp geoIp)
-        {
-            return _collection.InsertAsync(geoIp);
-        }
-
-        public async ValueTask DeleteAsync(string id)
-        {
-            await _collection.DeleteOneAsync(new BsonDocument("_id", new BsonObjectId(id)));
         }
     }
 }
