4030683e13d66ada9fbc33c16476171a46f88625
Vatt <protossgamover@gmail.com>
2021-03-06 15:28:34 +0500

    WIP (#67)
    Co-authored-by: Vatt <gamover@outlook.com>

---
+++ tests/MongoDB.Client.Tests.Models/ReadonlyStruct.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Client.Bson.Serialization.Attributes;
+using System;
+
+namespace MongoDB.Client.Tests.Models
+{
+    [BsonSerializable]
+    public readonly partial struct ReadonlyStruct : IEquatable<ReadonlyStruct>
+    {
+        public readonly int IntField;
+        public readonly double DoubleField;
+        public readonly string StringField;
+        public ReadonlyStruct(int IntField, double DoubleField, string StringField)
+        {
+            this.IntField = IntField;
+            this.DoubleField = DoubleField;
+            this.StringField = StringField;
+        }
+
+        public bool Equals(ReadonlyStruct other)
+        {
+            return IntField == other.IntField && DoubleField.Equals(other.DoubleField) && StringField == other.StringField;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReadonlyStruct other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(IntField, DoubleField, StringField);
+        }
+    }
+}
