260a45103babca4fb55ada54b2c6a185259f7fe3
Vatt <gamover@outlook.com>
2021-03-13 01:44:09 +0500

    WIP

--- src/MongoDB.Client.Bson/Serialization/Attributes/BsonSerializableAttribute.cs
+++ src/MongoDB.Client.Bson/Serialization/Attributes/BsonSerializableAttribute.cs
@@ -5,7 +5,8 @@ namespace MongoDB.Client.Bson.Serialization.Attributes
     public enum GeneratorMode
     {
         Default = 1,
-        SwitchOperations = 2
+        SwitchOperations = 2,
+        ContextTree = 3
     }
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
     public sealed class BsonSerializableAttribute : Attribute
