1b264508306ee6d8d834a956678fec9981d471fc
Vatt <gamover@outlook.com>
2021-04-03 00:20:06 +0500

    remove experimental methods

--- src/MongoDB.Client.Bson.Generators/SyntaxGenerator/Generator/SerializerGenerator.Collection.cs
+++ src/MongoDB.Client.Bson.Generators/SyntaxGenerator/Generator/SerializerGenerator.Collection.cs
@@ -9,62 +9,6 @@ namespace MongoDB.Client.Bson.Generators.SyntaxGenerator.Generator
 {
     internal static partial class SerializerGenerator
     {
-        public class CollectionWroteContext
-        {
-            public ISymbol Declaration;
-            public ExpressionSyntax BsonNameReadOperationExpr;
-            public ObjectCreationExpressionSyntax LocalCollectionCreateExpr;
-            public ImmutableList<StatementSyntax>.Builder BsonValueReadOperation;
-            public SyntaxToken BsonTypeToken;
-            public SyntaxToken OutMessageToken;
-            public SyntaxToken TempCollection;
-        }
-        public class CollectionWriteContext
-        {
-            public ISymbol Declaration;
-            public ExpressionSyntax BsonNameWriteOperationExpr;
-            public ImmutableList<StatementSyntax>.Builder BsonValueWriteOperation;
-        }
-        public static BlockSyntax CollectionTryParseBody(CollectionWroteContext ctx)
-        {
-            var docLenToken = Identifier("collectionDocLength");
-            var unreadedToken = Identifier("collectionUnreaded");
-            var endMarkerToken = Identifier("collectionEndMarker");
 
-            return Block(
-                SimpleAssignExprStatement(ctx.OutMessageToken, DefaultLiteralExpr()),
-                VarLocalDeclarationStatement(ctx.TempCollection, ctx.LocalCollectionCreateExpr),
-                IfNotReturnFalse(TryGetInt32(IntVariableDeclarationExpr(docLenToken))),
-                VarLocalDeclarationStatement(unreadedToken, BinaryExprPlus(ReaderRemainingExpr, SizeOfInt32Expr)),
-                SF.WhileStatement(
-                    condition:
-                    BinaryExprLessThan(
-                        BinaryExprMinus(IdentifierName(unreadedToken), ReaderRemainingExpr),
-                        BinaryExprMinus(IdentifierName(docLenToken), NumericLiteralExpr(1))),
-                    statement:
-                    Block(
-                        IfNotReturnFalse(TryGetByte(VarVariableDeclarationExpr(ctx.BsonTypeToken))),
-                        IfNotReturnFalse(ctx.BsonNameReadOperationExpr),
-                        IfStatement(
-                            condition: BinaryExprEqualsEquals(ctx.BsonTypeToken, NumericLiteralExpr(10)),
-                            statement: Block(
-                                InvocationExprStatement(ctx.TempCollection, CollectionAddToken, Argument(DefaultLiteralExpr())),
-                                ContinueStatement
-                            )),
-                        ctx.BsonValueReadOperation
-                    )),
-                IfNotReturnFalse(TryGetByte(VarVariableDeclarationExpr(endMarkerToken))),
-                IfStatement(
-                    BinaryExprNotEquals(endMarkerToken, NumericLiteralExpr((byte)'\x00')),
-                    Block(Statement(SerializerEndMarkerException(ctx.Declaration, IdentifierName(endMarkerToken))))),
-                SimpleAssignExprStatement(IdentifierName(ctx.OutMessageToken), ctx.TempCollection),
-                ReturnTrueStatement
-            );
-        }
-
-        public static BlockSyntax CollectionWriteBody(CollectionWroteContext ctx)
-        {
-            return default;
-        }
     }
 }
