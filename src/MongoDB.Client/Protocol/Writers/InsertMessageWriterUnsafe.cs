401d7fd8904db86e7a6e3d112b2f0b22735b5c01
Vatt <gamover@outlook.com>
2022-11-14 23:43:12 +0500

    Writers

--- src/MongoDB.Client/Protocol/Writers/InsertMessageWriterUnsafe.cs
+++ src/MongoDB.Client/Protocol/Writers/InsertMessageWriterUnsafe.cs
@@ -9,7 +9,7 @@ using MongoDB.Client.Protocol.Messages;
 namespace MongoDB.Client.Protocol.Writers
 {
     internal class InsertMessageWriterUnsafe<T> : IMessageWriter<InsertMessage<T>>
-    //where T : IBsonSerializer<T>
+        where T : IBsonSerializer<T>
     {
         public unsafe void WriteMessage(InsertMessage<T> message, IBufferWriter<byte> output)
         {
@@ -48,8 +48,7 @@ namespace MongoDB.Client.Protocol.Writers
 
             foreach (var item in message.Items)
             {
-                //T.WriteBson(ref writer, item);
-                SerializerFnPtrProvider<T>.WriteFnPtr(ref writer, item);
+                T.WriteBson(ref writer, item);
             }
 
             writer.Commit();
--- src/MongoDB.Client/Protocol/Writers/ReplyBodyWriter.cs
+++ src/MongoDB.Client/Protocol/Writers/ReplyBodyWriter.cs
@@ -6,13 +6,12 @@ using MongoDB.Client.Protocol.Core;
 namespace MongoDB.Client.Protocol.Writers
 {
     public class ReplyBodyWriter<T> : IMessageWriter<T>
-    //where T : IBsonSerializer<T>
+        where T : IBsonSerializer<T>
     {
         public unsafe void WriteMessage(T message, IBufferWriter<byte> output)
         {
             var writer = new BsonWriter(output);
-            //T.WriteBson(ref writer, message);
-            SerializerFnPtrProvider<T>.WriteFnPtr(ref writer, message);
+            T.WriteBson(ref writer, message);
         }
     }
 }
