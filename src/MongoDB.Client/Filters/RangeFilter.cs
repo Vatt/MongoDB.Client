19c6026e6f8d23dd3ff1cd9a5b1ae0eff6e3a83a
Vatt <gamover@outlook.com>
2023-10-14 04:52:08 +0500

    WIP

--- src/MongoDB.Client/Filters/RangeFilter.cs
+++ src/MongoDB.Client/Filters/RangeFilter.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Client.Bson.Writer;
+﻿using MongoDB.Client.Bson;
+using MongoDB.Client.Bson.Writer;
 
 namespace MongoDB.Client.Filters
 {
@@ -9,23 +10,26 @@ namespace MongoDB.Client.Filters
     }
     public class RangeFilter<T> : Filter
     {
-        private string _propertyName;
-        private T[] _values;
-        private RangeFilterType _type;
+        public string PropertyName { get; protected set; }
+        public T[] Values { get; protected set; }
+        public RangeFilterType Type { get; protected set; }
         public RangeFilter(string propertyName, T[] values, RangeFilterType type)
         {
-            _propertyName = propertyName;
-            _values = values;
-            _type = type;
+            PropertyName = propertyName;
+            Values = values;
+            Type = type;
         }
 
         public override void Write(ref BsonWriter writer)
         {
             var checkpoint = writer.Written;
-
             var reserved = writer.Reserve(sizeof(int));
+            writer.WriteBsonType(BsonType.Document);
+            writer.WriteName(PropertyName);
+            var checkpoint1 = writer.Written;
+            var reserved1 = writer.Reserve(sizeof(int));
 
-            switch (_type)
+            switch (Type)
             {
                 case RangeFilterType.In:
                     writer.Write_Type_Name(4, "$in"u8);
@@ -39,6 +43,8 @@ namespace MongoDB.Client.Filters
             
 
             WriteInner(ref writer);
+            writer.WriteByte((byte)'\x00');
+            reserved1.Write(writer.Written - checkpoint1);
             writer.WriteByte(0);
 
             reserved.Write(writer.Written - checkpoint);
@@ -48,15 +54,15 @@ namespace MongoDB.Client.Filters
         {
             var checkpoint = writer.Written;
             var reserved = writer.Reserve(4);
-            for (int i = 0; i < _values.Length; i++)
+            for (int i = 0; i < Values.Length; i++)
             {
-                var item = _values[i];
+                var item = Values[i];
 
                 var typeReserved = writer.Reserve(sizeof(byte));
                 writer.WriteName(i);
                 writer.WriteGeneric(item, ref typeReserved);
             }
-
+            
             writer.WriteByte(0);
             var docLength = writer.Written - checkpoint;
             reserved.Write(docLength);
---
+++ tests/MongoDB.Client.Tests/Client/FilterTests.cs
@@ -0,0 +1,78 @@
+﻿using MongoDB.Client.Bson.Document;
+using MongoDB.Client.Bson.Serialization.Attributes;
+using MongoDB.Client.Tests.Models;
+using Xunit;
+
+namespace MongoDB.Client.Tests.Client
+{
+    [BsonSerializable]
+    public partial record FilterTestRecord(int IntProp, string StringProp, double DoubleProp, long LongProp, decimal DecimalProp);
+
+    class Wrapper
+    {
+        public WrappedArray WrappedArrayField = new();
+        public WrappedArray WrappedArrayProperty { get; } = new();
+        public WrappedInt32 WrappedInt32 { get; } = new();
+    }
+    class WrappedArray
+    {
+        public int[] FieldArray = new[] { 1, 2, 3, 4, 5 };
+        public int[] PropertyArray { get; } = new[] { 5, 4, 3, 2, 1 };
+    }
+    class WrappedInt32
+    {
+        public int Value { get; } = 1;
+    }
+
+    public class FilterTests : ClientTestBase
+    {
+
+        private static int[] arr = new int[] { 1, 2, 3 };
+        private static bool boolVar = false;
+        private static Wrapper wrapper { get; set; } = new();
+
+        [Fact]
+        public async Task TestFilters()
+        {
+            var client = await CreateStandaloneClient(1);
+            var db = client.GetDatabase(DB);
+            var collection = db.GetCollection<FilterTestRecord>("FilterTestCollection" + Guid.NewGuid());
+            Memory<FilterTestRecord> data = new[]
+            {
+                new FilterTestRecord(1, "1", 1, 1, 1),
+                new FilterTestRecord(2, "2", 2, 2, 2),
+                new FilterTestRecord(3, "3", 3, 3, 3),
+                new FilterTestRecord(4, "4", 4, 4, 4)
+            };
+
+            await InsertAsync(data.ToArray(), collection);
+            
+            var result1 = await collection.Find(x => x.IntProp == 1).SingleOrDefaultAsync();
+            var result2 = await collection.Find(x => x.StringProp == "2").SingleOrDefaultAsync();
+            var result3 = await collection.Find(x => x.DoubleProp == 3).SingleOrDefaultAsync();
+            var result4 = await collection.Find(x => x.DecimalProp == 4).SingleOrDefaultAsync();
+            var result5 = await collection.Find(x => wrapper.WrappedInt32.Value == x.IntProp).SingleOrDefaultAsync();
+            var result6 = await collection.Find(x => x.IntProp == wrapper.WrappedInt32.Value).SingleOrDefaultAsync();
+            var result7 = await collection.Find(x => x.IntProp < wrapper.WrappedInt32.Value).SingleOrDefaultAsync();
+            var result8 = await collection.Find(x => x.IntProp > wrapper.WrappedInt32.Value).ToListAsync();
+            var result9 = await collection.Find(x => x.IntProp >= wrapper.WrappedInt32.Value).ToListAsync();
+            var result10 = await collection.Find(x => x.IntProp <= wrapper.WrappedInt32.Value).ToListAsync();
+            var result11 = await collection.Find(x => arr.Contains(x.IntProp)).ToListAsync();
+            var result12 = await collection.Find(x => arr.Contains(x.IntProp) == false).ToListAsync();
+            var result13 = await collection.Find(x => arr.Contains(x.IntProp) != boolVar).ToListAsync();
+            Assert.Equal(1, result1.IntProp);
+            Assert.Equal("2", result2.StringProp);
+            Assert.Equal(3, result3.DoubleProp);
+            Assert.Equal(4, result4.DecimalProp);
+            Assert.Equal(1, result6.IntProp);
+            Assert.Null(result7);
+            Assert.Equal(3, result8.Count);
+            Assert.Equal(4, result9.Count);
+            Assert.Single(result10);
+            Assert.Equal(3, result11.Count);
+            Assert.Single(result12);
+            Assert.Equal(3, result13.Count);
+            await collection.DropAsync();
+        }
+    }
+}
